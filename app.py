84508b57fbb4899396a0a83feae6e07526b3caf3
usmanlakhani <usmanlakhani@hotmail.com>
2025-02-20 01:38:31 -0500

    We added some UI elements

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import streamlit as st 
 
-st.write("Hello World 3")
+st.title("Simple Calculator")
+
+num1 = st.number_input('Enter 1st number', value=0.0)
+num2 = st.number_input('Enter 2nd number', value=0.0)
+
+operation = st.selectbox("Choose what you want to do with the 2 numbers entered",["+","-","*","/"])
