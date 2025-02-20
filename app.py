c7121dd0f80160043a2673cd5d823d4ea356b252
usmanlakhani <usmanlakhani@hotmail.com>
2025-02-20 01:57:00 -0500

    Revert "We changed the label"
    This reverts commit 66243e8b82284fd67b5358120b300dc52a477d11.

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st 
 
-st.title("Personal Information Form")
+st.title("Simple Calculator")
 
+num1 = st.number_input('Enter 1st number', value=0.0)
+num2 = st.number_input('Enter 2nd number', value=0.0)
+
+operation = st.selectbox("Choose what you want to do with the 2 numbers entered",["+","-","*","/"])
