66243e8b82284fd67b5358120b300dc52a477d11
usmanlakhani <usmanlakhani@hotmail.com>
2025-02-20 01:55:25 -0500

    We changed the label

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 import streamlit as st 
 
-st.title("Simple Calculator")
+st.title("Personal Information Form")
 
-num1 = st.number_input('Enter 1st number', value=0.0)
-num2 = st.number_input('Enter 2nd number', value=0.0)
-
-operation = st.selectbox("Choose what you want to do with the 2 numbers entered",["+","-","*","/"])
